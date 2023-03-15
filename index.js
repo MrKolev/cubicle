335110a2c503df550740193e4a41683ec9da86d6
MrKolev <100051403+MrKolev@users.noreply.github.com>
2023-03-15 14:41:38 +0200

    extract express to config

--- index.js
+++ index.js
@@ -1,14 +1,9 @@
 import express from "express";
 import { config } from "./config/config.js";
-import { engine } from 'express-handlebars';
+import { setupExpress } from "./config/express.js";
+
 
 const app = express();
-app.engine('hbs', engine());
-app.set('view engine', 'hbs');
-
-app.use(express.static("public"))
-app.get("/", (req, res) => {
-    res.render("home", {layout: false})
-});
+setupExpress(app);
 
 app.listen(config.PORT, ()=> console.log(`Server is running on port:${config.PORT}...`));
