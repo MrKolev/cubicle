ee082708c13ef9b8c29ad4c6e264075c0867ce0e
MrKolev <100051403+MrKolev@users.noreply.github.com>
2023-03-15 15:00:05 +0200

    add router

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 import express from "express";
 import { config } from "./config/config.js";
 import { setupExpress } from "./config/express.js";
+import { routers } from "./routes.js";
 
 
 const app = express();
 setupExpress(app);
+app.use(routers)
+
+
 
 app.listen(config.PORT, ()=> console.log(`Server is running on port:${config.PORT}...`));
