d144efd56c996dc49e54bb3fdf3591a7ca7c00e7
MrKolev <100051403+MrKolev@users.noreply.github.com>
2023-03-15 14:35:46 +0200

    add view and home

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 import express from "express";
 import { config } from "./config/config.js";
+import { engine } from 'express-handlebars';
 
 const app = express();
+app.engine('hbs', engine());
+app.set('view engine', 'hbs');
+
+app.use(express.static("public"))
+app.get("/", (req, res) => {
+    res.render("home", {layout: false})
+});
 
 app.listen(config.PORT, ()=> console.log(`Server is running on port:${config.PORT}...`));
