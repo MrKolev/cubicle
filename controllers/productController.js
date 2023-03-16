878f2aa88cf792e426df0db80b15f7b06c5ffe80
MrKolev <100051403+MrKolev@users.noreply.github.com>
2023-03-16 17:38:20 +0200

    update home page and create bd

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,22 +1,44 @@
 import { Router } from 'express';
+import { getAll, productService } from '../services/productService.js';
+
+
 const router = Router();
 
 
 router.get("/", (req, res) => {
-    res.render("home", { title: "Home" })
+    res.render("home", { title: "Home", products: getAll() })
 })
 
 router.get("/create", (req, res) => {
-    res.render("create",{ title: "Create" })
+    res.render("create", {
+        title: "Create", 
+        name: true,
+        description: true,
+        imageUrl: true
+    })
 })
 
-router.post("/create",(req, res) => {
-    console.log(req.body);
+router.post("/create", (req, res) => {
+    // validation input
+    const data = req.body
+    const { name, description, imageUrl} = data
+    if (!name || !description || !imageUrl) {
+        return res.render("create", {
+            title: "Create",
+            name: name,
+            description: description,
+            imageUrl: imageUrl,
+            data
+        })
+    }
+
+    productService(data);
     
+    res.redirect("/products")
 })
 
 router.get("/details/:productId", (req, res) => {
-    res.render("details",{ title: "Product Details" })
+    res.render("details", { title: "Product Details" })
 })
 
 export { router as productController };
