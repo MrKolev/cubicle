e2f8f009aaafa2a2a85684bceb90a06d91db5ac1
MrKolev <100051403+MrKolev@users.noreply.github.com>
2023-03-30 12:03:01 +0300

    update helper

--- controllers/helpers/helperProduct.js
+++ controllers/helpers/helperProduct.js
@@ -1,7 +1,12 @@
+import isEmpty from "validator/lib/isemail";
+import isURL from "validator/lib/isemail";
+
+
+
 export function validateProductInput(req, res, next) {
     let { name, description, imageUrl } = req.body
     try {
-        if (!name || name == " " || !description || description == " " || !imageUrl) {
+        if (isEmpty(name) || isEmpty(description) || isEmpty(imageUrl)) {
             throw { message: 'All fields must be filled.' };
         }
         if (name.length < 2) {
@@ -12,7 +17,20 @@ export function validateProductInput(req, res, next) {
             description = false;
             throw { message: 'Description must be at least 2 characters.' };
         };
-        if (!(/^https?/.test(imageUrl))) {
+        if (isURL(imageUrl), {
+            protocols: ['http', 'https', 'ftp'],
+            require_tld: true,
+            require_host: true,
+            allow_underscores: false,
+            host_whitelist: false,
+            host_blacklist: false,
+            allow_trailing_dot: false,
+            allow_protocol_relative_urls: false,
+            allow_fragments: true,
+            allow_query_components: true,
+            disallow_auth: false,
+            validate_length: true
+        }) {
             imageUrl = false;
             throw { message: 'Image URL must be a valid.' };
         };
